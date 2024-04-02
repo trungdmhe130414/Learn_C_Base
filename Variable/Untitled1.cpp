7ada6b5b393fe2d96112d3d146d2619ed010cbf3
minhtrung130414@gmail.com <minhtrung130414@gmail.com>
2024-04-02 22:18:56 +0700

    đẩy fle demo

---
+++ Variable/Untitled1.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+//// so nguyen
+//int : 4byte
+//long long : 8byte
+//// so thuc
+//float : 4 byte
+//double : 8 byte
+//// ky tu
+//char : 1 byte
+int main(){
+//	printf("Hello world ! \n");
+//	int a = 100;
+//	printf("%d" , a);
+//	printf("\n");
+//	float b = 1.233123;
+//	printf("%f" , (int)b);
+//	printf("\n");
+//	printf("%f" , b);
+//	printf("\n");
+//	printf("%f %d" , b , a);
+	int c;
+	printf("Enter c :" );
+	scanf("%d" , &c);
+	printf("c : %d" , c );
+//	int h ;
+//	int j ;
+//	int f ;
+//	scanf("%d%d%d" , &h , &j , &f);
+//	printf("%d%d%d" , h,j,f );
+//	int a;
+//	printf("Nhap a :" );
+//	scanf("%d" , &a);
+//	int b = a >= 18;
+//	printf("%d" , b );
+// https://www.youtube.com/watch?v=vpqMmfkSAMo&list=PLux-_phi0Rz2TB5D16sJzy3MgOht3IlND&index=1&ab_channel=28tech
+	return 0;
+}
